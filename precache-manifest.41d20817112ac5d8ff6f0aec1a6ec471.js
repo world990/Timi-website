0ccdda9a03f59c1b9f0f3ec60a49d0342229b509
world990 <342721572@qq.com>
2020-12-23 17:59:34 +0800

    deploy

---
+++ precache-manifest.41d20817112ac5d8ff6f0aec1a6ec471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11268c97e659a237c85a",
+    "url": "/Timi-website/css/app.6cea18e1.css"
+  },
+  {
+    "revision": "31aafe64c120cc6923c7bd4b5d2ae741",
+    "url": "/Timi-website/index.html"
+  },
+  {
+    "revision": "11268c97e659a237c85a",
+    "url": "/Timi-website/js/app.0c6c74f3.js"
+  },
+  {
+    "revision": "dd9f861f8fda37c3444d",
+    "url": "/Timi-website/js/chunk-vendors.7850edf6.js"
+  },
+  {
+    "revision": "5d458ad480ce73daaa73e5644f89459c",
+    "url": "/Timi-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Timi-website/robots.txt"
+  }
+]);
